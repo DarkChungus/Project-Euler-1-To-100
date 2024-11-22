74d3d4c3fcd541facbce09919e69505ca47a990d
godchuungus <96650932+DarkChungus@users.noreply.github.com>
2024-11-22 14:57:29 +0545

    Problem 5.py

---
+++ Problem 5.py
@@ -0,0 +1,7 @@
+# There is absolutely no programming here, if you want to do it very fast.
+# Just find the prime factors of each number(which is very easy to do by hand)
+# And multiply all of them together. Here the result will be,
+
+print(2**4 * 3**2 * 5 * 7 * 11 * 13 * 17 * 19)
+
+# Output: 232792560
